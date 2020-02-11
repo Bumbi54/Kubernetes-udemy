c12ad3c485f2a9fb08f2935bd8ef3f1cafa03a4c
root <ivan.drmic@verso.hr>
2020-02-11 12:53:02 +0100

    New branch change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          2. Edit <code>src/App.js</code> and save to reload. Hi there! ?
+          3. Edit <code>src/App.js</code> and save to reload. Hi there! ?
         </p>
         <a
           className="App-link"
